56a9f8b76ceaa356cd88a5b31fafa60e7b124b93
ChengHuan <rm0l485@hotmail.com.tw>
2018-03-29 13:21:27 +0800

    Bolizx
    Make it more useful.

--- 小說.py
+++ 小說.py
@@ -1,10 +1,15 @@
 import requests
 import urllib.parse 
 from bs4 import BeautifulSoup
+import time
 
-INDEX = 'https://ck101.com/forum.php?mod=forumdisplay&fid=3419&orderby=views&typeid=2399&orderby=views&typeid=2399&filter=reply&page=1'
+start = time.time()
+INDEX = 'https://ck101.com/forum-3419-1.html'
 headers = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 5.2) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.2.149.27 Safari/525.13'}
 #headers: 將要求包裝成瀏覽器之後送出的方法，在此為包裝成Chrome
+keyWord = ''
+limitt, limittt = 0,0
+
 def ListTheRow (url):
 	page = requests.get(url,headers = headers)
 	respond = BeautifulSoup(page.text, 'html.parser')
@@ -13,15 +18,19 @@ def ListTheRow (url):
 	for name in soup :
 		Title = name.find('a','s xst').get('title')
 		Like = name.find('span','thankNum').getText()
+		see = name.find('span','viewNum').getText()
+		Like = Like.replace('K','00').replace('.','').replace(' ','')
+		see = see.replace('K','00').replace('.','').replace(' ','')
 		try :
-			if ('異界'in Title and int (Like) > 100 or 'K' in Like):
-				member= {'Likes':Like, 'Name': Title,}
+			global keyWord 
+			if ( keyWord in Title and int (Like) >= limitt and int(see) >= limittt):
+				member= {'Likes':Like, 'Name': Title, 'view': see}
 				LikeMost.append(member)
 		except :
 			continue
 	
 	nextPage = respond.find('a','nxt').get('href')
-
+	
 	return LikeMost, nextPage
 
 def NewPage(PageNumber):
@@ -34,8 +43,14 @@ def NewPage(PageNumber):
 	return allPosts
 
 if __name__ == '__main__':
-	pages = 10
+	pages = int(input('想搜尋的頁數為：'))
+	keyWord = input('關鍵字為：')
+	limitt = int(input('感謝數：'))
+	limittt = int(input('觀看數：'))
 	for Page in range(1,pages+1):
 		print ('第',Page,'頁：')
 		for post in NewPage(Page):
-			print (post['Likes'], post['Name'])
+			print (post['Likes'], post['Name'], post['view'])
+	end = time.time()
+	elapsed = end - start
+	print ("Time taken: ", round(elapsed,3), "seconds.")
