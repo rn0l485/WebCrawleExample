43ed8d0be445c5414c6ae519f7f0bd5cba68c386
ChengHuan <rm0l485@hotmail.com.tw>
2018-04-17 21:25:38 +0800

    Make it traditionalize

--- url.py
+++ url.py
@@ -1,6 +1,7 @@
 import requests 
 import time
 from bs4 import BeautifulSoup
+from hanziconv import HanziConv
 
 url = input ('please type in the url.')
 headers = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 5.2) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.2.149.27 Safari/525.13'}
@@ -8,24 +9,27 @@ headers = {'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 5.2) AppleWebKit/52
 page = requests.get(url,headers = headers)
 respond = BeautifulSoup(page.text, 'html.parser')
 soup = respond.find_all('div','row article-dual-body-item')
-english, translate = [],[]
+english, TW = [],[]
 for text in soup:
 	EC = text.find_all('div','col-lg-6')
 	english.append(EC[0].getText())
-	for trans in EC:
-		translate.append(trans.getText())
+	TW.append(HanziConv.toTraditional(EC[1].getText()))
+
+trans = []
+for E in range(len(english)):
+	trans.append(english[E])
+	trans.append(TW[E])
 
 localtime = time.asctime( time.localtime(time.time()) )
 time = localtime+'.txt'
 time2 = localtime+'_translate.txt'
-f = open(time, 'w', encoding = 'UTF-8')
-for sentance in english:
-	f.write(sentance+'\n\n')
-f.close()
+with open(time, 'w', encoding = 'UTF-8') as f:
+	for sentance in english:
+		f.write(sentance+'\n\n')
 
-f1 = open(time2, 'w', encoding = 'UTF-8')
-for sentance in translate:
-	f1.write(sentance+'\n\n')
-f1.close()
+with open(time2, 'w', encoding = 'UTF-8') as f1:
+	for sentance in trans:
+		f1.write(sentance+'\n\n')
+
 
 
